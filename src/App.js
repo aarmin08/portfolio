025068c09f9ff3cbfa004b4805a127497ab76e6c
aarmin <aarmin.bhatnagar@pathways.in>
2021-02-17 17:24:09 +0530

    adding sanity studios

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("well");
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my portfolio website! <code>Aarmin</code>.
         </p>
+        <p>I will work on this further!</p>
+        <a className="App-link" href="https://www.youtube.com/channel/UCBvKcxR39NQ9J61MFZSgw2Q" target="_blank" rel="noopener noreferrer">My youtube channel</a>
         <a
           className="App-link"
           href="https://reactjs.org"
